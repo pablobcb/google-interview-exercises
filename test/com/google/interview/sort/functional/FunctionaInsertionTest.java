a43760078279c93d2bf4006175f0f3f8cc96bdd9
Pablo Botelho <pablo.bcb@gmail.com>
2016-01-29 13:28:53 -0200

    Functional Insertion Sort Tests Failing

---
+++ test/com/google/interview/sort/functional/FunctionaInsertionTest.java
@@ -0,0 +1,39 @@
+package com.google.interview.sort.functional;
+
+import com.google.interview.sort.TestUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FunctionaInsertionTest extends TestUtils{
+
+    @Test(expected = AssertionError.class)
+    public void comparingWithNullShouldThrowAnException() throws Exception {
+        assertArrayEquals(null, FunctionaInsertion.sort(this.ORDERED));
+    }
+
+    @Test
+    public void comparingEmptyArraysShouldSucceed() throws Exception {
+        assertArrayEquals(FunctionaInsertion.sort(this.EMPTY), this.EMPTY);
+    }
+
+    @Test
+    public void comparingSingleElementsArrayShouldSucceed() throws Exception {
+        assertArrayEquals(FunctionaInsertion.sort(new int[]{1}), this.UNIT);
+    }
+
+    @Test
+    public void comparingTwoElementsArrayShouldSucceed() throws Exception {
+        assertArrayEquals(FunctionaInsertion.sort(new int[]{252, 111}), this.TWO_ELEMENTS_ARRAY);
+    }
+
+    @Test
+    public void arrayShouldBeOrderedAfterInsertionSort() throws Exception {
+        assertArrayEquals(FunctionaInsertion.sort(this.UNORDERED), this.ORDERED);
+    }
+
+    @Test
+    public void orderedArrayShouldStillBeOrderedAfterInsertion() throws Exception {
+        assertArrayEquals(FunctionaInsertion.sort(this.ORDERED), this.ORDERED);
+    }
+}
